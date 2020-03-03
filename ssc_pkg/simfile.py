1d6cdcc43c9d1cbb43a96b7840019aef7bfac3af
andrewipark <andrewipark@users.noreply.github.com>
2020-03-03 09:39:11 -0600

    Change structure of simfile.py skeletons

--- ssc_pkg/simfile.py
+++ ssc_pkg/simfile.py
@@ -1,31 +1,15 @@
 from decimal import Decimal
-from pathlib import Path
-from typing import Mapping, Optional, Tuple
+from pathlib import PurePosixPath
+from typing import List, Mapping, Optional, Tuple
 
 import attr
 
 from . import notedata
 
-_TimingPosition = Decimal
+_TimingPosition = Decimal # somewhat tied to SM
 
 
-@attr.s(auto_attribs=True)
-class Chart:
-	'''Mutable POD representing a complete chart'''
-	meter: Optional[int] = None
-	difficulty: Optional[str] = None # sane default since it is the only one allowed multiple times
-
-	# TODO how do .sm files handle the one lone field they are allowed to have
-	credit: Optional[str] = None
-	description: Optional[str] = None
-	chart_name: Optional[str] = None
-
-	chart_style: Optional[str] = None
-
-	# radar values intentionally ignored because they are derived from note data, not a property of it
-
-	notes: notedata.NoteData = attr.Factory(notedata.NoteData)
-
+# class definitions
 
 @attr.s(auto_attribs=True)
 class TimingData:
@@ -35,53 +19,97 @@ class TimingData:
 		hit: int
 		miss: int
 
-	offset: _TimingPosition = Decimal(0)
-	preview_start: _TimingPosition = Decimal(0)
-	preview_length: _TimingPosition = Decimal(0)
+	# sm, minimum information
 	bpm: Mapping[_TimingPosition, Decimal] = attr.Factory(lambda: {Decimal(0): Decimal(120)})  # common default value
+	offset: _TimingPosition = Decimal(0)
 	stops: Mapping[_TimingPosition, Decimal] = attr.Factory(dict)
 
-	# begin SSC only data
-	delays: Mapping[_TimingPosition, Decimal] = attr.Factory(dict)
-	warps: Mapping[_TimingPosition, Decimal] = attr.Factory(dict)
-	time_signatures: Mapping[_TimingPosition, Tuple[int, int]] = attr.Factory(dict)
-	tick_counts: Mapping[_TimingPosition, int] = attr.Factory(dict)
-	combo_multipliers: Mapping[_TimingPosition, ComboMultiplier] = attr.Factory(dict)
-	speeds: Mapping[_TimingPosition, Tuple[Decimal, Decimal, str]] = attr.Factory(dict)
-	scrolls: Mapping[_TimingPosition, Decimal] = attr.Factory(dict)
-	fakes: str = '' # TODO
+	# ssc
+	delays: Optional[Mapping[_TimingPosition, Decimal]] = None
+	warps: Optional[Mapping[_TimingPosition, Decimal]] = None
+	time_signatures: Optional[Mapping[_TimingPosition, Tuple[int, int]]] = None
+	tick_counts: Optional[Mapping[_TimingPosition, int]] = None
+	combo_multipliers: Optional[Mapping[_TimingPosition, ComboMultiplier]] = None
+	speeds: Optional[Mapping[_TimingPosition, Tuple[Decimal, Decimal, str]]] = None
+	scrolls: Optional[Mapping[_TimingPosition, Decimal]] = None
+	fakes: Optional[str] = None # TODO unsupported
 
 	labels: Mapping[_TimingPosition, str] = attr.Factory(dict)
-	background_changes: Mapping[_TimingPosition, Path] = attr.Factory(dict)
-	keysounds: str = '' # TODO
-	attacks: str = '' # TODO
+
+	# sm
+	preview_start: _TimingPosition = Decimal(0)
+	preview_length: _TimingPosition = Decimal(0)
+	display_bpm: Optional[str] = None # TODO probably want a separate class
+
+	# rarely used, sm
+	background_changes: Mapping[_TimingPosition, str] = attr.Factory(dict) # TODO unsupported, = separated
+	foreground_changes: Mapping[_TimingPosition, str] = attr.Factory(dict) # TODO unsupported, = separated
+
+	keysounds: Optional[str] = None # TODO unsupported, poorly documented
+	attacks: Optional[str] = None # TODO unsupported
+
+
+@attr.s(auto_attribs=True)
+class Chart:
+	'''Mutable POD representing a complete chart'''
+
+	game_type: str = 'unknown' # semantic chart type
+
+	# difficulty data
+	meter: Optional[int] = None # sm: 1
+	difficulty: Optional[str] = 'Edit' # 'Edit' is the only one allowed multiple times
+
+	# descriptor fields
+	# TODO how do .sm files handle the one lone field they are allowed to have?
+	credit: Optional[str] = None
+	description: Optional[str] = None
+	chart_name: Optional[str] = None
+
+	chart_style: Optional[str] = None
+
+	# radar values intentionally ignored because they are derived from note data, not a property of it
+
+	timing_data: Optional[TimingData] = None
+
+	notes: notedata.NoteData = attr.Factory(notedata.NoteData)
 
 
 @attr.s(auto_attribs=True)
 class Simfile:
-	# variables from MSD data
-	title: str
+	'''Mutable POD representing an entire simfile'''
+
+	# common information
+	title: str = ''
 	subtitle: Optional[str] = None
 	artist: Optional[str] = None
 	title_transliterated: Optional[str] = None
 	subtitle_transliterated: Optional[str] = None
 	artist_transliterated: Optional[str] = None
 	genre: Optional[str] = None
-	origin: Optional[str] = None
 	credit: Optional[str] = None
+	music: Optional[PurePosixPath] = None
 
-	music: Optional[Path] = None
-	banner: Optional[Path] = None
-	background: Optional[Path] = None
-	cd_title: Optional[Path] = None
+	# sm resources
+	banner: Optional[PurePosixPath] = None
+	background: Optional[PurePosixPath] = None
+	lyrics: Optional[PurePosixPath] = None
+	cd_title: Optional[PurePosixPath] = None
 
-	# begin SSC only data
-	preview_video: Optional[Path] = None
-	jacket: Optional[Path] = None
-	cd_image: Optional[Path] = None
-	disc_image: Optional[Path] = None
-	lyrics: Optional[Path] = None
+	# ssc resources
+	preview_video: Optional[PurePosixPath] = None
+	jacket: Optional[PurePosixPath] = None
+	cd_image: Optional[PurePosixPath] = None
+	disc_image: Optional[PurePosixPath] = None
 
-	selectable: str = 'YES' # occasionally not a bool in 3.95. Just let it be.
+	# ssc, not a resource and no one uses it
+	origin: Optional[str] = None
 
-	additional_parameters: Mapping[str, str] = {}
+	# sm, not really relevant (on default SM5 installs)
+	selectable: str = 'YES'
+
+	timing_data: TimingData = attr.Factory(TimingData)
+
+	charts: List[Chart] = attr.Factory(list)
+
+	def is_split_timing(self) -> bool:
+		return any(c.timing_data for c in self.charts)
