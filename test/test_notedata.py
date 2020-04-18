55e8594837dfc521e580d73837613a5ac5fde310
andrewipark <andrewipark@users.noreply.github.com>
2020-04-18 15:41:23 -0500

    self.the -> self.simple

--- test/test_notedata.py
+++ test/test_notedata.py
@@ -6,7 +6,7 @@ from fractions import Fraction
 class TestNoteDataSimple(unittest.TestCase):
 
 	def setUp(self):
-		self.the = notedata.sm_to_notedata(
+		self.simple = notedata.sm_to_notedata(
 			# empty measure 0, staircase measure 1
 			'0000\n0000\n0000\n0000\n,\n1000\n0100\n0010\n0001\n,\n'
 			'0001\n0010\n0010\n1000\n0100\n0000\n0001\n0029\n'
@@ -20,65 +20,65 @@ class TestNoteDataSimple(unittest.TestCase):
 		])
 
 	def test_len(self):
-		self.assertEqual(len(self.the), 11)
+		self.assertEqual(len(self.simple), 11)
 		self.assertEqual(len(self.long_jack), self.long_jack_length)
 
 	def test_contains(self):
-		self.assertFalse(0 in self.the)
-		self.assertFalse(2 in self.the)
-		self.assertTrue(4 in self.the)
-		self.assertTrue(Fraction(17, 2) in self.the)
-		self.assertFalse(Fraction(35, 4) in self.the)
+		self.assertFalse(0 in self.simple)
+		self.assertFalse(2 in self.simple)
+		self.assertTrue(4 in self.simple)
+		self.assertTrue(Fraction(17, 2) in self.simple)
+		self.assertFalse(Fraction(35, 4) in self.simple)
 		self.assertTrue(self.long_jack_interval * 35 in self.long_jack)
 		self.assertFalse(self.long_jack_interval * Fraction(37, 12) in self.long_jack)
 
 	def test_getitem(self):
-		self.assertEqual(self.the[4], '1000')
-		self.assertEqual(self.the[Fraction(23, 2)], '0029')
+		self.assertEqual(self.simple[4], '1000')
+		self.assertEqual(self.simple[Fraction(23, 2)], '0029')
 
 	def test_getitem_invalid(self):
 		# valid on others, but not this one
-		self.assertRaises(IndexError, lambda: self.the[0])
-		self.assertRaises(IndexError, lambda: self.the[1])
-		self.assertRaises(IndexError, lambda: self.the[2])
-		self.assertRaises(IndexError, lambda: self.the[Fraction(21, 2)])
-		self.assertRaises(IndexError, lambda: self.the[69])
-		self.assertRaises(IndexError, lambda: self.the[Fraction(-2, 3)])
+		self.assertRaises(IndexError, lambda: self.simple[0])
+		self.assertRaises(IndexError, lambda: self.simple[1])
+		self.assertRaises(IndexError, lambda: self.simple[2])
+		self.assertRaises(IndexError, lambda: self.simple[Fraction(21, 2)])
+		self.assertRaises(IndexError, lambda: self.simple[69])
+		self.assertRaises(IndexError, lambda: self.simple[Fraction(-2, 3)])
 
 		# never valid
-		self.assertRaises(TypeError, lambda: self.the[None])
+		self.assertRaises(TypeError, lambda: self.simple[None])
 
 	def test_getitem_slicing(self):
 		# easy empty cases
-		self.assertEqual(len(self.the[0:0]), 0)
-		self.assertEqual(len(self.the[0:4]), 0)
-		self.assertEqual(len(self.the[4:4]), 0)
-		self.assertEqual(len(self.the[20:]), 0)
-		self.assertEqual(len(self.the[:-1]), 0)
+		self.assertEqual(len(self.simple[0:0]), 0)
+		self.assertEqual(len(self.simple[0:4]), 0)
+		self.assertEqual(len(self.simple[4:4]), 0)
+		self.assertEqual(len(self.simple[20:]), 0)
+		self.assertEqual(len(self.simple[:-1]), 0)
 
 		# non empty cases
-		self.assertEqual(self.the, self.the[:])
-		self.assertEqual(self.the[6:], self.the[6:12])
-		self.assertEqual(self.the[:8], self.the[2:8])
+		self.assertEqual(self.simple, self.simple[:])
+		self.assertEqual(self.simple[6:], self.simple[6:12])
+		self.assertEqual(self.simple[:8], self.simple[2:8])
 
 		# standard
 		start, stop = 5, Fraction(19, 2)
-		new_notedata = self.the[start:stop]
+		new_notedata = self.simple[start:stop]
 		self.assertEqual(len(new_notedata), 6)
-		self.assertEqual(self.the[8], '0001')
+		self.assertEqual(self.simple[8], '0001')
 		self.assertTrue(start in new_notedata)
 		self.assertFalse(stop in new_notedata)
 
 	def test_shift(self):
 		# reversible
-		self.assertEqual(self.the.shift(4).shift(-4), self.the)
-		self.assertEqual(self.the.shift(Fraction(13, 4)).shift(Fraction(-13, 4)), self.the)
+		self.assertEqual(self.simple.shift(4).shift(-4), self.simple)
+		self.assertEqual(self.simple.shift(Fraction(13, 4)).shift(Fraction(-13, 4)), self.simple)
 
-		self.assertEqual(self.the.shift(20)[24], '1000')
-		self.assertEqual(self.the.shift(Fraction(-3, 2))[Fraction(17, 2)], '0100')
+		self.assertEqual(self.simple.shift(20)[24], '1000')
+		self.assertEqual(self.simple.shift(Fraction(-3, 2))[Fraction(17, 2)], '0100')
 
 	def test_clear_range(self):
-		new_notedata = self.the.clear_range(5, 9)
+		new_notedata = self.simple.clear_range(5, 9)
 
 		self.assertFalse(2 in new_notedata)
 		self.assertTrue(4 in new_notedata)
@@ -102,7 +102,7 @@ class TestNoteDataSimple(unittest.TestCase):
 
 	def test_density(self):
 		self.assertEqual(
-			self.the.density(), [
+			self.simple.density(), [
 				notedata.DensityInfo(1, 4),
 				notedata.DensityInfo(Fraction(1, 2), 4),
 				notedata.DensityInfo(1, 1),
