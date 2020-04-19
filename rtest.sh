55f0522578b8a3820c15972d18cb32b493646020
andrewipark <andrewipark@users.noreply.github.com>
2020-04-19 18:13:15 -0500

    Improve testing
    rtest.sh: -Wdefault -Werror
    
    Make TravisCI use Bionic to avoid virtualenv errors from above

--- rtest.sh
+++ rtest.sh
@@ -3,12 +3,15 @@
 set -eux
 
 mypy ssc_pkg test
+
 # name of python binary varies thanks to PEP 394 (also see https://lwn.net/Articles/780737/)
 # try 'python3' to be completely unambiguous on Unix systems, but windows cygwin only does 'python'.
 if command -v python3 > /dev/null 2>&1; then
-	python3 -m unittest discover
+	PYTHONEXEC=python3
 else
-	python -m unittest discover
+	PYTHONEXEC=python
 fi
+
+$PYTHONEXEC -W error -m unittest discover
 
 flake8 --exit-zero ssc_pkg test
