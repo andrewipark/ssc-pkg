cc6e02a8ebb29f70cb6d2d3acd854285a2ae0e4f
andrewipark <andrewipark@users.noreply.github.com>
2020-05-26 10:48:56 -0500

    make: improve manager unit tests

--- test/make/test_manager.py
+++ test/make/test_manager.py
@@ -29,6 +29,10 @@ class TestManagerObj(unittest.TestCase):
 
 	GROUP_SIZE: ClassVar[int] = 4
 
+	def mgr_run(self, command: c.Command):
+		'''convenience method to run commands without a simfile attached'''
+		self.manager.run(command, Simfile())
+
 	def setUp(self):
 		self.manager = Manager()
 
@@ -41,7 +45,7 @@ class TestManagerObj(unittest.TestCase):
 		]
 		with self.assertLogs(self.manager.logger) as lm:
 			for item in collection:
-				self.manager.run(c.Pragma('echo', item), Simfile())
+				self.mgr_run(c.Pragma('echo', item))
 		self.assertEqual(len(lm.output), len(collection))
 		for i in range(len(collection)):
 			self.assertIn(str(collection[i]), lm.output[i])
@@ -52,16 +56,22 @@ class TestManagerObj(unittest.TestCase):
 	def test_run_pragma_raise(self):
 		pass # TODO
 
+	def test_run_pragma_callable(self):
+		'''if this test fails, a lot of the control structure tests are useless'''
+		ctx = _LogExecContext()
+		self.mgr_run(ctx.get_command(666666))
+		self.assertEqual(ctx.buf, [666666])
+
 	def test_run_pragma_invalid(self):
-		self.assertRaises(CommandError, lambda: self.manager.run(c.Pragma('NO', None), Simfile()))
+		self.assertRaises(CommandError, lambda: self.mgr_run(c.Pragma('NO', None)))
 
 	def test_run_group(self):
 		ctx_single = _LogExecContext()
 		for i in range(self.GROUP_SIZE):
-			self.manager.run(ctx_single.get_command(i), Simfile())
+			self.mgr_run(ctx_single.get_command(i))
 
 		ctx_group = _LogExecContext()
-		self.manager.run(c.Group(ctx_group.get_commands(range(self.GROUP_SIZE))), Simfile())
+		self.mgr_run(c.Group(ctx_group.get_commands(range(self.GROUP_SIZE))))
 
 		self.assertEqual(ctx_single.buf, ctx_group.buf)
 
@@ -71,15 +81,15 @@ class TestManagerObj(unittest.TestCase):
 		# basically the same as group, except now we have to define the function beforehand
 		ctx_single = _LogExecContext()
 		for i in range(self.GROUP_SIZE):
-			self.manager.run(ctx_single.get_command(i), Simfile())
+			self.mgr_run(ctx_single.get_command(i))
 
 		ctx_def = _LogExecContext()
 		commands = c.Group(ctx_def.get_commands(range(self.GROUP_SIZE)))
-		self.manager.run(c.Def('run_def_call_simple', commands), Simfile())
+		self.mgr_run(c.Def('run_def_call_simple', commands))
 
 		self.assertEqual([], ctx_def.buf)
 		for i in range(1, 6):
-			self.manager.run(c.Call('run_def_call_simple'), Simfile())
+			self.mgr_run(c.Call('run_def_call_simple'))
 			self.assertEqual(ctx_single.buf * i, ctx_def.buf)
 
 	def test_run_def_call_scope_visibility(self):
@@ -89,19 +99,19 @@ class TestManagerObj(unittest.TestCase):
 			c.Call('blah2')
 		]))
 
-		self.manager.run(define_blah, Simfile())
-		self.manager.run(c.Call('blah'), Simfile())
+		self.mgr_run(define_blah)
+		self.mgr_run(c.Call('blah'))
 		with ErrorIndex(self, CommandError, ['Call']):
-			self.manager.run(c.Call('blah2'), Simfile()) # not visible from outside 'blah'
+			self.mgr_run(c.Call('blah2')) # not visible from outside 'blah'
 
 	def test_run_call_invalid(self):
 		with ErrorIndex(self, CommandError, ['Call']):
-			self.manager.run(c.Call('not_existent_yet'), Simfile())
+			self.mgr_run(c.Call('not_existent_yet'))
 		with ErrorIndex(self, CommandError, ['Call']):
-			self.manager.run(c.Call('might_exist'), Simfile())
+			self.mgr_run(c.Call('might_exist'))
 
-		self.manager.run(c.Def('might_exist', c.Group([])), Simfile())
-		self.manager.run(c.Call('might_exist'), Simfile())
+		self.mgr_run(c.Def('might_exist', c.Group([])))
+		self.mgr_run(c.Call('might_exist'))
 
 	def test_run_let(self):
 		value_test = {
@@ -113,9 +123,20 @@ class TestManagerObj(unittest.TestCase):
 		}
 
 		for name, value in value_test.items():
-			self.manager.run(c.Let(name, value), Simfile())
+			self.mgr_run(c.Let(name, value))
 			self.assertEqual(self.manager.frames[-1].variables[name], value)
 
 	def test_run_let_scope_shadowing(self):
 		'''Test that objects defined within scopes shadow objects defined outside the scope'''
-		pass # TODO
+		n = 'gamma'
+		self.mgr_run(c.Let(n, 1))
+		self.assertEqual(self.manager.lookup(n), 1)
+
+		# shadowed assignment in group
+		self.mgr_run(c.Group([
+			c.Let(n, 2),
+			c.Pragma('callable', [lambda m: self.assertEqual(m.lookup(n), 2)]),
+		]))
+
+		# no longer there outside group
+		self.assertEqual(self.manager.lookup(n), 1)
